0cbc148b3d2ed21124b55b5baa0aaaf5fdb0a24f
YUAN LIU <lymybox@gmail.com>
2024-02-03 22:39:12 -0800

    20240203-002

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import logo from '../images/Logo .svg'
 
 const Nav = () => {
     return (
         <nav>
-            <h1>jsdsdcsdcsdcfsd</h1>
+            <a href="/">
+                <img src={logo} alt="little lemon website"/>
+            </a>
         </nav>
     )
 };
