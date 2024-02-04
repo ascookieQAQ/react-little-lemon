4c458470fe0984bf33ba9f3fd971ee44485d4b99
YUAN LIU <lymybox@gmail.com>
2024-02-04 09:05:44 -0800

    20240204-002

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,20 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
     return (
         <header className='header'>
             <section>
-                <div>
+                <div className='banner'>
                     <h2>Little Lemon</h2>
                     <h3>Chicago</h3>
+                    <p>
+                        We are a family owned Mediterraneran restaurant, focused on traditional recipes served with a modern twist.
+                    </p>
+                    <Link to="/booking">
+                        <button>Reserve a Table</button>
+                    </Link>
+                    {/* <Link to="/booking"><button>Reserve a Table</button></Link> */}
                 </div>
             </section>
         </header>
