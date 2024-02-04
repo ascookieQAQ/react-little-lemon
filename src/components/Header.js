2b9a89ff342c2a7c6d083a102e758953523de09f
YUAN LIU <lymybox@gmail.com>
2024-02-04 08:38:56 -0800

    20240204-001

---
+++ src/components/Header.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <header className='header'>
+            <section>
+                <div>
+                    <h2>Little Lemon</h2>
+                    <h3>Chicago</h3>
+                </div>
+            </section>
+        </header>
+    );
+};
+
+export default Header;
